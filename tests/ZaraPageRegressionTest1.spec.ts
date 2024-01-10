c8cdff3b65af37235cf2cc26036bfee101d2c20b
Hasan Smajlović <smajlovic.hasan02@gmail.com>
2024-01-10 15:19:02 +0100

    Software Testing

--- tests/ZaraPageRegressionTest1.spec.ts
+++ tests/ZaraPageRegressionTest1.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
-import ZaraRegisterPage from "../pages/ZaraRegisterPage/ZaraRegisterPage";
+import test1 from "../RegressionTesting/test1";
 
 test('Zara register test', async ({page}) => {
-    const zaraPage = new ZaraRegisterPage(page);
+    const zaraPage = new test1(page);
     await zaraPage.openPage();
     await zaraPage.clickLogInButton();
     await zaraPage.clickRegisterButton();
@@ -12,10 +12,9 @@ test('Zara register test', async ({page}) => {
     await zaraPage.surname('Cale');
     await zaraPage.phonePrefix('+387');
     await zaraPage.phone('61413138');
-    await zaraPage.checkPrivacyCheckbox();
     await zaraPage.clickCreateAccount();
     await zaraPage.waitForPageLoad();
-    // await page.pause();
+    await page.pause();
 
     // Assert that the current URL contains the expected path
     expect(await zaraPage.getCurrentUrl()).toContain('https://www.zara.com/ba/en/error/invalid-session');
---
+++ tests/ZaraPageRegressionTest2.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import test2 from "../RegressionTesting/test2";
+import test1 from '../RegressionTesting/test1';
+
+test('Verify Register with data that is invalid', async ({page}) => {
+    const zaraPage = new test2(page);
+    await zaraPage.openPage();
+    await zaraPage.clickLogInButton();
+    await zaraPage.clickRegisterButton();
+    await zaraPage.inputEmail('faksfakss65LudoAmudja.com');
+    await zaraPage.inputPassword('.-,$password#!"$');
+    await zaraPage.fillNameField('Mudja');
+    await zaraPage.surname('Cale');
+    await zaraPage.phonePrefix('+387');
+    await zaraPage.phone('61413138');
+    await zaraPage.checkPrivacyCheckbox();
+    await zaraPage.clickCreateAccount();
+    await zaraPage.waitForPageLoad();
+    await page.pause();
+
+    // Assertion for the email validation error message
+  const emailErrorMessage = page.locator('div[data-name="email"] .form-input-error');
+  await expect(emailErrorMessage).toContainText('Enter a valid e-mail address.');
+
+  // Assertion for the password validation error message
+  const passwordErrorMessage = page.locator('div[data-name="password"] .form-input-error');
+  await expect(passwordErrorMessage).toContainText('Enter a secure password: At least 8 characters long, containing uppercase and lowercase letters and numbers.');
+
+})
